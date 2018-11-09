e5fa8fc0f9112a4169746cb6b539207761fa4e01
Jonatan Machado <jonatanmachado35@gmail.com>
2018-11-08 23:38:54 -0200

    aula

---
+++ objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['desconto legal'] = 0.40 // evitar atributos com espaços
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'camise polo',
+    preco: 79.90
+}
+
+console.log(prod2)
