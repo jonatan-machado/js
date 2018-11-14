014930b06badfb1839c5c65275be84e2ed259588
Jonatan Machado <jonatanmachado35@gmail.com>
2018-11-14 17:41:00 -0200

    exeplos de if e switch

---
+++ estrutura de controle/switch.js
@@ -0,0 +1,22 @@
+const imprimirResultado = function (nota){
+  switch (Math.floor(nota)){
+    case 10:
+    case 9:
+      console.log('Quadro de Honra')
+      break
+    case 8: case 7:
+      console.log('aprovado')
+      break
+    case 6: case 5: case 4:
+      console.log('Recuparação')
+      break
+    case 3: case 2: case 1:
+      console.log('Reprovado')
+      break
+    default:
+      console.log('Nota Inválida')
+  }
+}
+
+imprimirResultado(10)
+imprimirResultado(20)
